305e2161d3d6adf7e4854d0d223fcc814b1c5741
Ju111 <SchneiderJu@gmx.net>
2018-04-03 22:31:27 +0200

    add login router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import LoginPage from './components/login/LoginPage'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to the Evaluator</h1>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to the Evaluator</h1>
+          </header>
+          <Route exact path="/login" component={ LoginPage } />
+          <Route exact path="/" render={ () => <Redirect to="/login" /> } />
+        </div>
+      </Router>
     );
   }
 }
