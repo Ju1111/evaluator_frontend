7298b8a20ff011ee5a424b420f1a55c7b317ee07
Ju111 <SchneiderJu@gmx.net>
2018-04-26 14:43:22 +0200

    move get jwt out of function

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -2,19 +2,19 @@ import { USER_LOGIN_SUCCESS, USER_LOGOUT } from '../actions/types'
 import { localStorageJwtKey } from '../constants'
 
 let initialState = null
+try {
+  const jwt = sessionStorage.getItem(localStorageJwtKey)
+  if (jwt) {
+    initialState = { jwt }
+  }
+}
+catch (e) {
+  console.log(`Error retrieving data from local storage`, e)
+}
 
 export default function (state = initialState, { type, payload }) {
 	switch (type) {
   		case USER_LOGIN_SUCCESS:
-      try {
-        const jwt = localStorage.getItem(localStorageJwtKey)
-        if (jwt) {
-          initialState = { jwt }
-        }
-      }
-      catch (e) {
-        console.log(`Error retrieving data from local storage`, e)
-      }
 			return payload
 
     case USER_LOGOUT:
